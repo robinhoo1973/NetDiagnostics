a2ea090728f1feb4b4bdce9cf6b49bfd25706197
robinhoo1973 <robinhoo1973@users.noreply.github.com>
2026-07-12 16:56:31 +0800

    chore: remove temp file

--- _temp_check.ps1
+++
@@ -1,7 +0,0 @@
-$lines = Get-Content -Path 'src/Diagnostics/Controller/TaskFactory.cpp'
-for ($i = 0; $i -lt $lines.Count; $i++) {
-    $l = $lines[$i]
-    if ($l -match '^\s*#\s*(if|ifdef|ifndef|elif|else|endif)') {
-        Write-Host ('{0,4}: {1}' -f ($i+1), $l.Trim())
-    }
-}
